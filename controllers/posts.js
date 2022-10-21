5100030f20d454be84cc9c6e49a6b4af93232de0
Brian Fuentes <Brian.Fuentes04@gmail.com>
2022-10-21 09:41:02 -0700

    added working changes

--- controllers/posts.js
+++ controllers/posts.js
@@ -4,8 +4,9 @@ const Post = require("../models/Post");
 module.exports = {
   getProfile: async (req, res) => {
     try {
-      const posts = await Post.find({ user: req.user.id });
-      res.render("profile.ejs", { posts: posts, user: req.user });
+      const posts = await Post.findById(req.params.id);
+      res.render("profile.ejs", { posts: posts, user: req.user }); 
+      console.log(posts)
     } catch (err) {
       console.log(err);
     }
@@ -40,7 +41,7 @@ module.exports = {
         user: req.user.id,
       });
       console.log("Post has been added!");
-      res.redirect("/profile");
+      res.redirect("/profile/:id");
     } catch (err) {
       console.log(err);
     }
@@ -68,9 +69,9 @@ module.exports = {
       // Delete post from db
       await Post.remove({ _id: req.params.id });
       console.log("Deleted Post");
-      res.redirect("/profile");
+      res.redirect("/profile/:id");
     } catch (err) {
-      res.redirect("/profile");
+      res.redirect("/profile/:id");
     }
   },
 };
