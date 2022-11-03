ff741ce000c8874b420dd7853923cd1529b7d89d
Brian Fuentes <Brian.Fuentes04@gmail.com>
2022-11-02 19:08:31 -0700

    linked client js file

--- public/js/main.js
+++ public/js/main.js
@@ -1,9 +1,37 @@
-const red = document.getElementById('addItem')
-red.addEventListener("click", turnRed);
-
-
-function turnRed() {
-  red.style.color = 'red';
-}
-
- console.log('Hello World!')
+// var price_field = document.querySelector('#price');
+// price_field.addEventListener('keyup', function() {
+//   formatCurrency(this);
+// });
+// price_field.addEventListener('blur', function() {
+//   formatCurrency(this, "blur");
+// });
+// function formatNumber(n) {
+//   return n.replace(/\D/g, "").replace(/\B(?=(\d{3})+(?!\d))/g, ",");
+// }
+// function formatCurrency(input, blur) {
+//   var input_val = input.value;
+//   if (input_val === "") { return; }
+//   var original_len = input_val.length;
+//   var caret_pos = input.selectionStart;
+//   if (input_val.indexOf(".") >= 0) {
+//     var decimal_pos = input_val.indexOf(".");
+//     var left_side = input_val.substring(0, decimal_pos);
+//     var right_side = input_val.substring(decimal_pos);
+//     left_side = formatNumber(left_side);
+//     right_side = formatNumber(right_side);
+//     if (blur === "blur") {
+//       right_side += "00";
+//     }
+//     right_side = right_side.substring(0, 2);
+//     input_val = "$" + left_side + "." + right_side;
+//   } else {
+//     input_val = formatNumber(input_val);
+//     input_val = "$" + input_val;
+//     if (blur === "blur") {
+//       input_val += ".00";
+//     }
+//   }
+//   input.value = input_val;
+//   var updated_len = input_val.length;
+//   caret_pos = updated_len - original_len + caret_pos;
+// }
